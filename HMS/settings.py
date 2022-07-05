c6a49535b8b93bbf19cd2ff264dde1f6045b1417
Sumithub3 <84029428+Sumithub3@users.noreply.github.com>
2022-07-05 17:02:53 +0530

    updations72378

--- HMS/settings.py
+++ HMS/settings.py
@@ -137,4 +137,6 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+django_heroku.settings(locals())
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
