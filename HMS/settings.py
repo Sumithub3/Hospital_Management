681db46a1516568884ed3bb4fe29aa5b484b7e0c
Sumithub3 <84029428+Sumithub3@users.noreply.github.com>
2022-07-05 16:33:20 +0530

    updates444

--- HMS/settings.py
+++ HMS/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = /images/
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
