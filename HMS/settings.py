aa2f2f2d5827f7be4751dcae9de7d9cfb5917fd7
Sumit <srhedaoo@gmail.com>
2022-07-06 23:22:46 +0530

    Extra Updations010

--- HMS/settings.py
+++ HMS/settings.py
@@ -1,7 +1,7 @@
 
 import os
 from pathlib import Path
-import django_heroku
+import django-on-heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-django_heroku.settings(locals())
+django-on-heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
